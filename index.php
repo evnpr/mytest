2202626aec3fff878fb2205a76a84f105ad62fa3
Ubuntu <ubuntu@ip-10-130-17-184.ap-southeast-1.compute.internal>
2012-09-26 07:48:28 +0000

    evnpr just edit /mytest/index.php

--- index.php
+++ index.php
@@ -4,13 +4,6 @@ unlink("../img/test.php");
 
 
 
-$ftp_server = "letspan.com";
-
-// set up a connection or die
-$conn_id = ftp_connect($ftp_server) or die("Couldn't connect to $ftp_server"); 
-var_dump($conn_id);
-$file = "/var/www/ls/upload/img/test.php";
-ftp_delete($conn_id, $file);
 ?>
 
 
