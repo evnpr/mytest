26881260c65882ac19176e72a8459ca0ca5e8461
Ubuntu <ubuntu@ip-10-130-17-184.ap-southeast-1.compute.internal>
2012-10-08 06:58:54 +0000

    evnpr just edit /mytest/index.php

--- index.php
+++ index.php
@@ -1,20 +1,3 @@
-<?php
-session_start();
-
-if ($_SESSION["thetreenamechild"] == 'drive E')
-    $_SESSION["thetreenamechild"] = 'drive F';
-else if ($_SESSION["thetreenamechild"] == 'drive D')
-	$_SESSION["thetreenamechild"] = 'drive E';
-else if ($_SESSION["thetreenamechild"] == 'drive C')
-	$_SESSION["thetreenamechild"] = 'drive D';
-else
-	$_SESSION["thetreenamechild"] = 'drive C';
-
-echo $_SESSION["thetreenamechild"] ;
-
-
-
-
-?>
+helloworld
 
 
