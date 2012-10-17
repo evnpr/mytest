27c4dfedb4cd1d90c1f68621e72f7ef08401db54
Ubuntu <ubuntu@ip-10-130-17-184.ap-southeast-1.compute.internal>
2012-10-17 09:19:05 +0000

    evnpr just edit /mytest/index.php

--- index.php
+++ index.php
@@ -1,8 +1,9 @@
 <?php
 
 
-$file = file_get_contents("https://facebook.com");
+$file = file_get_contents("http://news.detik.com/read/2012/10/17/160349/2065192/1148/jasad-norodom-sihanouk-tiba-di-kamboja?9911012);
 echo var_export($file, true);
+
 
 
 
