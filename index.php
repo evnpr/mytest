16041a60297ab85340a332a899ca451c8fc80b85
Ubuntu <ubuntu@ip-10-130-17-184.ap-southeast-1.compute.internal>
2012-10-23 07:51:44 +0000

    evnpr just edit /mytest/index.php

--- index.php
+++ index.php
@@ -137,7 +137,7 @@ pageTracker._trackPageview();
 
 <script>
 v = document.getElementById("uploadprogress").value
-if(v = 99){
+if(v == 99){
     document.getElementById("holder").style.background = "url(thankyou.png)"
 }
 
