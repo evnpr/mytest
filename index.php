6faea3df4ac9e1834beccf740e35c4af17cde222
Ubuntu <ubuntu@ip-10-130-17-184.ap-southeast-1.compute.internal>
2012-10-23 14:02:40 +0000

    adiarriansyah just edit /mytest/index.php

--- index.php
+++ index.php
@@ -140,7 +140,7 @@ pageTracker._trackPageview();
 <script>
 $(document).ready(
     
-v = document.getElementById("uploadprogress").value
+v = document.getElementById("uploadprogress").value;
 if(v == 100){
     document.getElementById("holder").style.background = "url(thankyou.png)";
     document.getElementById("progress").style.display="none";
