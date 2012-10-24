f9962509af94ab8ee4d3eefe7f2f948691ec93f8
Ubuntu <ubuntu@ip-10-130-17-184.ap-southeast-1.compute.internal>
2012-10-24 11:55:31 +0000

    adiarriansyah just edit /mytest/index.php

--- index.php
+++ index.php
@@ -26,7 +26,7 @@ progress:after { content: '%'; }
   <p id="filereader">File API & FileReader API not supported</p>
   <p id="formdata">XHR2's FormData is not supported</p>
   <p id="progress">XHR2's upload progress isn't supported</p>
-  <p>Upload progress: <progress id="uploadprogress" min="0" max="100" value="0" style:>0</progress></p>
+  <p>Upload progress: <progress id="uploadprogress" min="0" max="100" value="0" style:{visibility:hidden;}>0</progress></p>
 </article>
 
 <script>
