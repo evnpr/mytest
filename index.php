99851e9993648e93fa931b3cb5a84681cb599c3f
Ubuntu <ubuntu@ip-10-130-17-184.ap-southeast-1.compute.internal>
2012-11-06 03:34:02 +0000

    evnpr just edit /mytest/index.php

--- index.php
+++ index.php
@@ -1,7 +1,1 @@
-<pre id="typingTools" contentEditable="true" style="height: 400px; width: 900px; margin: 200 0 -200 200; background-color: transparent;"> 
-                WRITE HERE, double click to back to writing tools
-
-
-
-                </pre>
-
+<input type="text" />
